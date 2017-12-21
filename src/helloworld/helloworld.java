25a6e9cd518e0638cbba6611819b00fcc4a442a1
Administrator <Administrator@9NPNPTFPJTMNLLO>
2017-12-22 00:26:10 +0900

    하나를 뻄

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,6 +4,5 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Java");
-		System.out.println("Hello Um");
 	}
 }
